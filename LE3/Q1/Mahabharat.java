413af152d7692944d0713b66daaefe30d9243142
221B005 <221B005@juet.co.in>
2025-02-03 14:36:47 +0530

    LE3

---
+++ LE3/Q1/Mahabharat.java
@@ -0,0 +1,93 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+abstract class Bharatvanshi {
+    abstract void fight(); 
+}
+abstract class Pandav extends Bharatvanshi {
+    void obey() {
+        System.out.println(getClass().getSimpleName() + " is obedient.");
+    }
+
+    abstract void kind(); // Kindness varies, so making it abstract
+}
+class Arjun extends Pandav {
+    @Override
+    void fight() {
+        System.out.println("Arjun is a great warrior with archery skills.");
+    }
+
+    @Override
+    void kind() {
+        System.out.println("Arjun is kind-hearted.");
+    }
+}
+class Bheem extends Pandav {
+    @Override
+    void fight() {
+        System.out.println("Bheem is a great warrior with immense strength.");
+    }
+
+    @Override
+    void kind() {
+        System.out.println("Bheem is kind but a little less than Arjun.");
+    }
+}
+abstract class Kaurav extends Bharatvanshi {
+    @Override
+    void fight() {
+        System.out.println(getClass().getSimpleName() + " is a fighter.");
+    }
+
+    void obey() {
+        System.out.println(getClass().getSimpleName() + " is disobedient.");
+    }
+
+    void kind() {
+        System.out.println(getClass().getSimpleName() + " is cruel.");
+    }
+}
+class Duryodhan extends Kaurav {
+}
+
+class Vikarn extends Kaurav {
+    @Override
+    void obey() {
+        System.out.println("Vikarn is obedient.");
+    }
+
+    @Override
+    void kind() {
+        System.out.println("Vikarn is kind-hearted.");
+    }
+}
+public class Mahabharat {
+    public static void main(String[] args) {
+        Arjun arjun = new Arjun();
+        Bheem bheem = new Bheem();
+        Duryodhan duryodhan = new Duryodhan();
+        Vikarn vikarn = new Vikarn();
+
+        System.out.println("\n--- Pandavs ---");
+        arjun.fight();
+        arjun.obey();
+        arjun.kind();
+
+        bheem.fight();
+        bheem.obey();
+        bheem.kind();
+
+        System.out.println("\n--- Kauravs ---");
+        duryodhan.fight();
+        duryodhan.obey();
+        duryodhan.kind();
+
+        vikarn.fight();
+        vikarn.obey();
+        vikarn.kind();
+    }
+}
---
+++ LE3/Q3/DuckGame.java
@@ -0,0 +1,136 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+interface SwimBehavior {
+    default void swim() {
+        System.out.println("All ducks can swim.");
+    }
+}
+interface FlyBehavior {
+    void fly();
+}
+interface QuackBehavior {
+    void quack();
+}
+class FlyWithWings implements FlyBehavior {
+    public void fly() {
+        System.out.println("I can fly with my wings.");
+    }
+}
+
+class NoFly implements FlyBehavior {
+    public void fly() {
+        System.out.println("I can't fly.");
+    }
+}
+class Quack implements QuackBehavior {
+    public void quack() {
+        System.out.println("Quack! Quack!");
+    }
+}
+
+class Squeak implements QuackBehavior {
+    public void quack() {
+        System.out.println("Squeak! Squeak!");
+    }
+}
+
+class Mute implements QuackBehavior {
+    public void quack() {
+        System.out.println("...");
+    }
+}
+abstract class Duck implements SwimBehavior {
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+
+    // Constructor to assign behaviors dynamically
+    Duck(FlyBehavior flyBehavior, QuackBehavior quackBehavior) {
+        this.flyBehavior = flyBehavior;
+        this.quackBehavior = quackBehavior;
+    }
+    void performFly() {
+        flyBehavior.fly();
+    }
+
+    void performQuack() {
+        quackBehavior.quack();
+    }
+
+    abstract void display();
+}
+class RubberDuck extends Duck {
+    RubberDuck() {
+        super(new NoFly(), new Squeak()); 
+    }
+
+    void display() {
+        System.out.println("I'm a Rubber Duck.");
+    }
+}
+
+class WoodenDuck extends Duck {
+    WoodenDuck() {
+        super(new NoFly(), new Mute()); 
+    }
+
+    void display() {
+        System.out.println("I'm a Wooden Duck.");
+    }
+}
+
+class RedHeadDuck extends Duck {
+    RedHeadDuck() {
+        super(new FlyWithWings(), new Quack()); 
+    }
+
+    void display() {
+        System.out.println("I'm a RedHead Duck.");
+    }
+}
+
+class LakeDuck extends Duck {
+    LakeDuck() {
+        super(new FlyWithWings(), new Quack()); 
+    }
+
+    void display() {
+        System.out.println("I'm a Lake Duck.");
+    }
+}
+public class DuckGame {
+    public static void main(String[] args) {
+        Duck rd = new RubberDuck();
+        Duck wd = new WoodenDuck();
+        Duck rhd = new RedHeadDuck();
+        Duck ld = new LakeDuck();
+
+        System.out.println("\n--- Duck Show ---");
+        rd.display();
+        rd.swim();
+        rd.performFly();
+        rd.performQuack();
+
+        System.out.println("\n---");
+        wd.display();
+        wd.swim();
+        wd.performFly();
+        wd.performQuack();
+
+        System.out.println("\n---");
+        rhd.display();
+        rhd.swim();
+        rhd.performFly();
+        rhd.performQuack();
+
+        System.out.println("\n---");
+        ld.display();
+        ld.swim();
+        ld.performFly();
+        ld.performQuack();
+    }
+}
